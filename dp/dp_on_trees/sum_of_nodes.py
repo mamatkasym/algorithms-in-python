3d79518a3442782277f00fd77f0e8134ba6326ba
mamatkasym <mm.kalandar@gmail.com>
2021-09-16 11:55:36 +0600

    added dp on trees

---
+++ dp/dp_on_trees/sum_of_nodes.py
@@ -0,0 +1,9 @@
+"""
+Consider a tree in which every node has a positive value written on it.
+The task is to select several nodes from the tree (you may also select none),
+so that the sum of values of the nodes you select is maximized, and no two adjacent nodes are selected.
+The desired solution has complexity O(n).
+"""
+
+def solve():
+    pass
