ed6c13739a41737ba1c846b4c0e52d48dd0d8c60
mamatkasym <mm.kalandar@gmail.com>
2021-09-16 11:55:02 +0600

    added mo's algorithm

---
+++ miscellaneous/mo's_algorithm.py
@@ -0,0 +1,5 @@
+"""
+You are given array Arr of length N and Q queries.
+Each query is represented by two numbers L and R,
+and it asks you to compute some function Func with subarray Arr[L..R] as its argument.
+"""
