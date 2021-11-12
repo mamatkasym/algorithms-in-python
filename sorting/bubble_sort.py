0477d13bf213776e2953006d4ac41c8ea3e548dc
mamatkasym <mm.kalandar@gmail.com>
2021-11-12 21:19:11 +0600

    added bubble sort

---
+++ sorting/bubble_sort.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+def bubble_sort(arr: List[int]) -> None:
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j+1] < arr[j]:
+                arr[j+1], arr[j] = arr[j], arr[j+1]
+
+
+a = [2, 4, 1, 7, 3, 4, 2, 6, 3]
+bubble_sort(a)
+print(a)
