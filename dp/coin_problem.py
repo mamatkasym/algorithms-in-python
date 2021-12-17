64a60671c4b62b85629f15d103f632ce02ea60db
mamatkasym <mm.kalandar@gmail.com>
2021-12-17 13:46:53 +0600

    added function to calculate number of ways to form target sum

--- dp/coin_problem.py
+++ dp/coin_problem.py
@@ -1,14 +1,11 @@
-"""
-Given a set
-of coin values coins = {c1, c2,..., ck} and a target sum of money n, our task is to
-form the sum n using as few coins as possible
-"""
-
 
 def minimum_coins(coins: list[int], target: int) -> int:
     """
+    Given a set
+    of coin values coins = {c1, c2,..., ck} and a target sum of money n, our task is to
+    form the sum n using as few coins as possible
     :param coins: set of integer values of coins
-    :param target: target number to form
+    :param target: target number to form using coins
     :return: number of minimum number of coins to form target
     TC: O(len(coins) * target)
     MC: O(len(coins) * target)
@@ -35,3 +32,19 @@ def minimum_coins(coins: list[int], target: int) -> int:
 # *************************************************************
 
     return dp[target]
+
+
+def number_of_ways(coins: list[int], target: int) -> int:
+    """
+    The total number of ways to produce a sum x using the coins.
+    :param coins: set of integer values of coins
+    :param target: target number to form using coins
+    :return: number of different ways to form target using coins
+    """
+    count = [0] * (target + 1)
+    for i in range(1, target + 1):
+        for coin in coins:
+            if i - coin >= 0:
+                count[i] += count[i - coin]
+
+    return count[target]
