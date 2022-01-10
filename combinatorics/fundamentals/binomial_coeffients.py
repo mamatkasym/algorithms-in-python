1258c091522e5692313a138c0785e829d8b4c4ca
mamatkasym <mm.kalandar@gmail.com>
2022-01-10 19:12:22 +0600

    added more binomial algorithms

--- combinatorics/fundamentals/binomial_coeffients.py
+++ combinatorics/fundamentals/binomial_coeffients.py
@@ -1,3 +1,6 @@
+import math
+
+from algebra.modular_inverse import using_extended_euclidean_algorithm
 
 
 def timer(fn):
@@ -15,7 +18,12 @@ def timer(fn):
 
 
 @timer
-def naive(n, k):
+def naive(n: int, k: int) -> int:
+    """
+    TC: O(n)
+    MC: O(1)
+    Inefficient or overflows for large n
+    """
     res = 1
     for i in range(n - k + 1, n+1):
         res *= i
@@ -25,13 +33,45 @@ def naive(n, k):
 
 
 @timer
-def improved(n, k):
+def improved(n: int, k: int) -> int:
+    """
+    TC: O(k)
+    MC: O(1)
+    Inefficient or overflows for large n
+    """
     res = 1.0
     for i in range(1, k+1):
         res = res * (n-k+i) / i
     return int(res + 0.01)
 
 
-if __name__ == '__main__':
-    naive(2000, 1000)
-    improved(2000, 1000)
+def pascals_triangle(n: int, k: int) -> int:
+    """
+    TC: O(n*k)
+    MC: O(n*k)
+    """
+    # dp[i][j] = C(i, j)
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    dp[0][0] = 1
+    for i in range(1, n + 1):
+        dp[i][0] = dp[i][i] = 1
+        for j in range(1, i):
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1]
+
+    return dp[n][k]
+
+
+def binomial_coefficients_modulo_m(n: int, k: int, m: int) -> int:
+    def binomial_coefficient(dn: int, dk: int) -> int:
+        """ TC: O(log(m))"""
+        return factorial[n] * using_extended_euclidean_algorithm(factorial[k] * factorial[n - k] % m, m) % m
+
+    # Pre compute factorials
+    factorial = [0] * (n + 1)
+    factorial[0] = 1
+    for i in range(1, n + 1):
+        factorial[i] = factorial[i-1] * i % m
+
+    return binomial_coefficient(n, k)
+
+
