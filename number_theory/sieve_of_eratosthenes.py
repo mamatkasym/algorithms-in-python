c088268af0a3eccaa201ac71caa72abe3bc49eb1
mamatkasym <mm.kalandar@gmail.com>
2022-01-19 12:45:02 +0600

    added optimized sieve of eratosthenes algorithm

--- number_theory/sieve_of_eratosthenes.py
+++ number_theory/sieve_of_eratosthenes.py
@@ -1,11 +1,9 @@
-"""
-Time complexity: O(n*log(log(n)))
-Memory: O(n)
-"""
-
-
-def primes(n):
-    """ Return prime numbers until n """
+def primes(n: int) -> list[int]:
+    """
+    Return prime numbers less than n
+    Time complexity is O(n*log(log(n)))
+    Memory complexity is O(n)
+    """
 
     is_prime = [True] * n
     prime_numbers = []
@@ -13,8 +11,27 @@ def primes(n):
     for i in range(2, n):
         if is_prime[i]:
             prime_numbers.append(i)
-            j = i + i
+            j = i * i
             while j < n:
                 is_prime[j] = False
                 j += i
     return prime_numbers
+
+
+def primes_optimized(n: int) -> list[int]:
+    """ Needs less operations than previous function """
+    is_prime = [True] * n
+    is_prime[0] = is_prime[1] = False
+    i = 0
+    while i * i < n:
+        if is_prime[i]:
+            j = i * i
+            while j < n:
+                is_prime[j] = False
+                j += i
+        i += 1
+    return [i for i in range(n) if is_prime[i]]
+
+
+def test():
+    assert primes_optimized(14) == primes(14) == [2, 3, 5, 7, 11, 13]
